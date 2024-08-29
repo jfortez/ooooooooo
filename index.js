836cd23a942c6dc6572d3a70abc854d0f74708b1
Jean Caiza <jeanmcm@hotmail.com>
2024-08-29 10:03:00 -0500

    wrqww

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import http from "http";
 import mysql from "mysql2/promise";
+
 const server = http.createServer(async (req, res) => {
   if (req.method === "GET" && req.url === "/user") {
     res.statusCode = 200;
@@ -9,12 +10,14 @@ const server = http.createServer(async (req, res) => {
     return;
   }
   if (req.method === "GET" && req.url === "/db") {
-    const connection = await mysql.createConnection({
-      host: process.env.MYSQL_HOST || "localhost",
-      user: "root",
-      password: "root",
-      database: "test",
-    });
+    const access = {
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      port: process.env.MYSQL_PORT,
+    };
+    const connection = await mysql.createConnection(access);
     const result = await connection.query("SELECT 2+2 AS result");
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
